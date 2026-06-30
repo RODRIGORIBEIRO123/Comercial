59b9759fdc16027b7b17629f2c39ac4cd3378835
RODRIGORIBEIRO123 <rodrigo.ribeiro@siarcon.com.br>
2026-06-30 10:29:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1312,7 +1312,7 @@ elif st.session_state.menu_selecionado == "🔌 Levantamento de Automação":
         df_cfr = pd.DataFrame([{"Item / Equipamento": k, "Valor Atual (R$)": st.session_state.precos_banco.get(k, 0.0)} for k in lista_cfr if k in st.session_state.precos_banco])
         edited_cfr = st.data_editor(df_cfr, use_container_width=True, hide_index=True, key="ed_cfr")
         
-       if st.button("💾 Salvar Novos Preços no Banco de Dados", type="primary"):
+        if st.button("💾 Salvar Novos Preços no Banco de Dados", type="primary"):
             alterou_algo = False
             novos_historicos = []
             edited_total = pd.concat([edited_geral, edited_siemens, edited_mercato, edited_cfr], ignore_index=True)
@@ -1349,11 +1349,8 @@ elif st.session_state.menu_selecionado == "🔌 Levantamento de Automação":
                     st.session_state.data_precos_atualizada = data_hora_agora
                     st.cache_data.clear()
                     
-                    # --- CORREÇÃO APLICADA AQUI ABAIXO ---
                     st.toast("✅ Base de preços atualizada com sucesso!", icon="💾")
-                    st.rerun() 
-                    # -------------------------------------
-                    
+                    st.rerun()
                 except Exception as e: st.error(f"Erro ao salvar: {e}")
 
     # FUNÇÃO INTELIGENTE DE EAP (Extrai as tags separadas para exportação)
